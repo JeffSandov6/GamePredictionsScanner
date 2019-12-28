b925e53cf62cf979bb10f4bd23f59b0e5cc024c0
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2019-12-28 17:56:39 -0600

    stuff

--- Websites/WinnersAndWhiners.py
+++ Websites/WinnersAndWhiners.py
@@ -16,24 +16,38 @@ class WinnersAndWhiners:
 
     def getPrediction(self, team1, team2):
         predictionSiteLink = self.getPredictionSiteLink(team1, team2)
-        print(predictionSiteLink)
+        
+        predictions = self.getAllAvailablePredictions(predictionSiteLink)
 
+        return predictions #right now, returning a list of all predictions
+
+    
+    def getAllAvailablePredictions(self, predictionSiteLink):
         pageRequest = requests.get(predictionSiteLink)
-        pageTree = html.fromstring(self.pageRequest.content)
+        pageTree = html.fromstring(pageRequest.content)
+
+
+        # temp = pageTree.xpath('//*[@id="full-game-side"]') #correct one 
+        # temp = pageTree.get_element_by_id("full-game-side")
+
+        pickClassElemList = pageTree.find_class("pick") #this finds all classes named pick and returns a list of its elements
+        predictions = []
+        for curElem in pickClassElemList:
+            for curPrediction in curElem.getchildren():
+                predictions.append(curPrediction.text)
+
+        return predictions
 
     
     def getPredictionSiteLink(self, team1, team2):
         rowOfGamesElem = self.pageTree.xpath('//*[@id="ww-vue-app"]/div[3]/div/div[2]/div[2]/div') #returns elems after div class="row"
+        potentialGameLinks = set()
 
-        potentialGameLinks = set() #this should be a set
-        # someLinks = []
         for x in rowOfGamesElem:
             links = x.iterlinks()
-
             for i in links:
                 if team1 in i[2] and i[2][0:5] == "https":
                     potentialGameLinks.add(i[2])
-                    # someLinks.append(i[2])
 
         return self.getCorrectLink(potentialGameLinks)
 
@@ -51,22 +65,3 @@ class WinnersAndWhiners:
     def hasCorrectFormat(self, gameLink):
         firstDateNum = gameLink[42:43] #gets the first char right after ncaab/ in the link
         return firstDateNum.isnumeric() #if its a number (part of a date), this is the correct link
-
-
-
-
-########################################
-
-            # next = x.find_class("title")
-
-            # print("the type is")
-            # print(type(next))
-            
-            # for i in next:
-            #     try:
-            #         print(i.text_content())
-            #     except:
-            #         print("skipping the ones w out it")
-
-
-            
