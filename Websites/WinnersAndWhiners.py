a221c11132dc9af361e704b836ecb5c4761646c0
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2020-01-20 16:08:33 -0600

    Finished up StatSalt page and introduced a feature where each bet type is specified now. ALso reduced some of the hardcoding that was involved early. The natural next step is to add a new betting page

--- Websites/WinnersAndWhiners.py
+++ Websites/WinnersAndWhiners.py
@@ -5,8 +5,10 @@ from lxml import html
 class WinnersAndWhiners:
 
     sportWebsiteMap = {
+        "siteHomePage" : "http://winnersandwhiners.com/",
         "ncaab" : "https://winnersandwhiners.com/games/ncaab",
-        "nfl" : "https://winnersandwhiners.com/games/nfl/"
+        "nfl" : "https://winnersandwhiners.com/games/nfl/",
+        "nba" : "https://winnersandwhiners.com/games/nba/"
     }
 
     def __init__(self, sport):
@@ -29,15 +31,20 @@ class WinnersAndWhiners:
         pageRequest = requests.get(predictionSiteLink)
         pageTree = html.fromstring(pageRequest.content)
 
-
         # temp = pageTree.xpath('//*[@id="full-game-side"]') #correct one 
         # temp = pageTree.get_element_by_id("full-game-side")
 
         pickClassElemList = pageTree.find_class("pick") #this finds all classes named pick and returns a list of its elements
         predictions = []
         for curElem in pickClassElemList:
+            parent = curElem.getparent()
+            currentBetType = parent.attrib['id']  #returns the bet type
+
             for curPrediction in curElem.getchildren():
-                predictions.append(curPrediction.text)
+                prediction = curPrediction.text.replace('Prediction', currentBetType)
+
+                # predictions.append(curPrediction.text)
+                predictions.append(prediction.strip())
 
         return predictions
 
@@ -49,7 +56,9 @@ class WinnersAndWhiners:
         for x in rowOfGamesElem:
             links = x.iterlinks()
             for i in links:
-                if team1 in i[2] and i[2][0:5] == "https":
+                #the way the links object is returned, each object has 4 parts to it,
+                #where the second is the link
+                if (team1 in i[2]) and (team2 in i[2]) and (i[2][0:5] == "https"):
                     potentialGameLinks.add(i[2])
 
         return self.getCorrectLink(potentialGameLinks)
